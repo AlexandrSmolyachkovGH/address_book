c71fa26f4cf0cae1e1a864c6163035688668a1f4
AlexandrSmolyachkovGH <alexandr.smolyachkov@gmail.com>
2025-12-08 20:32:27 +0300

    feat: Build Dockerfile and CI conf. Add application part into docker-compose.

--- app/main.py
+++ app/main.py
@@ -59,11 +59,3 @@ async def get_root() -> dict:
             "redoc": "/redoc",
         },
     }
-
-
-if __name__ == '__main__':
-    uvicorn.run(
-        'app.main:app',
-        host="127.0.0.1",
-        port=8081,
-    )
